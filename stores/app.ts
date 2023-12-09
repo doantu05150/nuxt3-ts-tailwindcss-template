800ecd55cd2b20150c38eaf841d0ab174ad7582b
doantu05150 <doantufptu@gmail.com>
2023-12-09 11:18:29 +0700

    feat: config pinia

---
+++ stores/app.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+
+interface State {
+  theme: "dark" | "light";
+}
+
+export const useCounterStore = defineStore("app", {
+  state: (): State => ({ theme: "light" }),
+  getters: {},
+  actions: {
+    setTheme(theme?: "dark" | "light") {
+      if (theme) {
+        this.theme = theme;
+      } else {
+        this.theme = this.theme === "dark" ? "light" : "dark";
+      }
+    },
+  },
+});
