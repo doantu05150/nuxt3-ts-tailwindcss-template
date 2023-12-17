ea19abcd195827af9b94b553d7387df2cc0ab2f1
doantu05150 <doantufptu@gmail.com>
2023-12-17 20:44:42 +0700

    feat: google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "nuxt-icons"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  modules: ["@pinia/nuxt", "nuxt-icons"],
+  googleFonts: {
+    families: {
+      Poppins: [200, 300, 400, 500, 600, 700, 800, 900],
     },
   },
 });
